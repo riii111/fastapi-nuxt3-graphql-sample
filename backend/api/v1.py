5947ebf256bc534708d98473981426b7c2680b67
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-12 16:30:56 +0900

    不要な処理を削除

--- backend/api/v1.py
+++ backend/api/v1.py
@@ -1,4 +1,4 @@
-from api.endpoints import auth, batch, book, user
+from api.endpoints import auth, book, user
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -6,4 +6,3 @@ api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/users", tags=["user"])
 api_router.include_router(book.router, prefix="/books", tags=["book"])
-api_router.include_router(batch.router, prefix="/batch", tags=["batch"])
