83ad84935512f919dcfc780d960a064190fcbda6
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-10-16 15:52:33 +0900

    対象IDのbookを取得するロジックを追加

--- backend/gql/resolvers.py
+++ backend/gql/resolvers.py
@@ -9,3 +9,10 @@ from .types import BookViewType
 async def get_books(info: Info[GraphQLContext, None]) -> List[BookViewType]:
     books = await info.context.book_usecase.get_all_books()
     return [BookViewType(**book.model_dump(exclude={"secret"})) for book in books]
+
+
+async def get_book_by_id(
+    info: Info[GraphQLContext, None], book_id: str
+) -> BookViewType:
+    book = await info.context.book_usecase.get_book_by_id(book_id)
+    return BookViewType(**book.model_dump(exclude={"secret"}))
--- backend/gql/router.py
+++ backend/gql/router.py
@@ -1,6 +1,6 @@
 import strawberry
 
-from .resolvers import get_books
+from .resolvers import get_books, get_book_by_id
 from .types import BookViewType
 
 
@@ -8,3 +8,4 @@ from .types import BookViewType
 @strawberry.type
 class Query:
     books: list[BookViewType] = strawberry.field(resolver=get_books)
+    book_by_id: BookViewType = strawberry.field(resolver=get_book_by_id)
